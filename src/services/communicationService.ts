ea917085271c335d994887ce7160b52f636c1c2f
johnpaul okoye <okoyejohnpaul15@gmail.com>
2024-09-11 09:59:13 +0100

    fix: add nodemailer credentials

--- src/services/communicationService.ts
+++ src/services/communicationService.ts
@@ -1,15 +1,29 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import SMTPTransport = require('nodemailer/lib/smtp-transport');
+
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  // Configure your email service here
-});
+const { EMAIL_USER, EMAIL_PASSWORD, EMAIL_FROM } = process.env;
+const transportOptions: SMTPTransport.Options = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: { 
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD, 
+    },
+    tls: {
+        rejectUnauthorized: true
+    }
+};
+
+const transporter = nodemailer.createTransport(transportOptions);
 
 export const sendThankYouMessage = async (email: string) => {
   const mailOptions = {
-    from: process.env.EMAIL_FROM || 'default@example.com',
+    from: EMAIL_FROM || 'default@example.com',
     to: email,
     subject: 'Thank You for Your Donations',
     text: 'Thank you for making multiple donations! Your generosity is greatly appreciated.'
